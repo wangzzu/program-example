7142b44c91ecb47485cb1da2b4ddbe9cb96c358e
wangzzu <wangm92@163.com>
2019-03-19 20:48:36 +0800

    modify flink-example and add several flink examples

--- flink-example/src/main/java/com/matt/test/table/WordCountTable.java
+++ flink-example/src/main/java/com/matt/test/table/WordCountTable.java
@@ -1,8 +1,67 @@
 package com.matt.test.table;
 
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.TableEnvironment;
+import org.apache.flink.table.api.java.BatchTableEnvironment;
+
 /**
- * @author matt
- * @date 2019-03-18 20:43
+ * Simple example for demonstrating the use of the Table API for a Word Count in Java.
+ *
+ * <p>This example shows how to:
+ * - Convert DataSets to Tables - Apply group, aggregate, select, and filter operations
  */
 public class WordCountTable {
+
+    // *************************************************************************
+    //     PROGRAM
+    // *************************************************************************
+
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.createCollectionsEnvironment();
+        BatchTableEnvironment tEnv = TableEnvironment.getTableEnvironment(env);
+
+        DataSet<WC> input = env.fromElements(
+            new WC("Hello", 1),
+            new WC("Ciao", 1),
+            new WC("Hello", 1));
+
+        Table table = tEnv.fromDataSet(input);
+
+        Table filtered = table
+            .groupBy("word")
+            .select("word, frequency.sum as frequency")
+            .filter("frequency = 2");
+
+        DataSet<WC> result = tEnv.toDataSet(filtered, WC.class);
+
+        result.print();
+    }
+
+    // *************************************************************************
+    //     USER DATA TYPES
+    // *************************************************************************
+
+    /**
+     * Simple POJO containing a word and its respective count.
+     */
+    public static class WC {
+        public String word;
+        public long frequency;
+
+        // public constructor to make it a Flink POJO
+        public WC() {}
+
+        public WC(String word, long frequency) {
+            this.word = word;
+            this.frequency = frequency;
+        }
+
+        @Override
+        public String toString() {
+            return "WC " + word + " " + frequency;
+        }
+    }
 }
+
--- flink-example/src/main/java/com/matt/test/worcount/SocketTextStreamWordCount.java
+++ flink-example/src/main/java/com/matt/test/worcount/SocketTextStreamWordCount.java
@@ -1,8 +1,51 @@
 package com.matt.test.worcount;
 
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
 /**
  * @author matt
  * @date 2019-03-18 20:06
  */
 public class SocketTextStreamWordCount {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("USAGE:\nSocketTextStreamWordCount <hostname> <port>");
+            return;
+        }
+
+        String hostname = args[0];
+        Integer port = Integer.parseInt(args[1]);
+
+        // set up the streaming execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        //获取数据
+        DataStreamSource<String> stream = env.socketTextStream(hostname, port);
+
+        //计数
+        SingleOutputStreamOperator<Tuple2<String, Integer>> sum = stream.flatMap(new LineSplitter())
+            .keyBy(0)
+            .sum(1);
+
+        sum.print();
+
+        env.execute("Java WordCount from SocketTextStream Example");
+    }
+
+    public static final class LineSplitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+        public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) {
+            String[] tokens = s.toLowerCase().split("\\W+");
+
+            for (String token : tokens) {
+                if (token.length() > 0) {
+                    collector.collect(new Tuple2<String, Integer>(token, 1));
+                }
+            }
+        }
+    }
 }
